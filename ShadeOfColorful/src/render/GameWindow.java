2756de8f76d109306f7fc56c0887a398148e781d
5730279821-TA <Mix77041721>
2015-11-17 13:49:39 +0700

    First Draft

---
+++ ShadeOfColorful/src/render/GameWindow.java
@@ -0,0 +1,38 @@
+package render;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameWindow extends JFrame{
+	private JPanel currentFrame;
+	
+	protected GameWindow(JPanel frame){
+		super("Shade Of Colorful");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		this.currentFrame=frame;
+		getContentPane().add(currentFrame);
+		pack();
+		this.setVisible(true);
+		this.currentFrame.requestFocus();
+	}
+	
+	protected void ChangeFrame(JPanel frame){
+		getContentPane().remove(currentFrame);
+		this.currentFrame=frame;
+		getContentPane().add(frame);
+		getContentPane().validate();
+		pack();
+		currentFrame.requestFocus();
+	}
+
+	protected JPanel getCurrentFrame() {
+		return currentFrame;
+	}
+
+	protected void setCurrentFrame(JPanel currentFrame) {
+		this.currentFrame = currentFrame;
+	}
+	
+	
+}
